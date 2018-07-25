d2f541b7193472705c64578391501d79614de52e
luketedford <41654907+luketedford@users.noreply.github.com>
2018-07-24 21:18:45 -0400

    Update lesson3.py

--- lesson3.py
+++ lesson3.py
@@ -109,6 +109,18 @@ print( 7 // 4 ) # without decimal - integer division
 # 85 'B' 
 # 90%+ = A, 80 <= B < 90
 
+score = int(input(enter your test score here))
+if score > 89
+  print('A')
+elif score > 79
+  print('B')
+elif score > 69
+  print('C')
+elif score > 59
+  print('D')
+else
+  print('F')
+
 
 
 # Quadratic formula 
